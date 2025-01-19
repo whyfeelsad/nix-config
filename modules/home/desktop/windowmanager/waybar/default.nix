eb240f3dc9dc252bdcea564c91843750cb16aa96
Aaron <the.sad@niceboy.org>
2025-01-19 20:08:11 +0800

    feat: update waybar

--- modules/home/desktop/windowmanager/waybar/default.nix
+++ modules/home/desktop/windowmanager/waybar/default.nix
@@ -1,162 +1,263 @@
-{
-  lib,
-  pkgs,
-  ...
-}: let
-  palette = import ./palette.nix;
-in {
+{pkgs, ...}: {
   programs.waybar = {
     enable = true;
-    systemd.enable = true;
-    settings = [
-      {
-        layer = "top";
-        position = "top";
-        modules-left = [
-          "niri/workspaces"
-          "wlr/taskbar"
-        ];
-        modules-center = ["clock"];
-        modules-right = [
-          "tray"
-          "idle_inhibitor"
-          "memory"
-          "backlight"
-          "pulseaudio"
-          "battery"
-        ];
+    package = pkgs.waybar;
+    settings = {
+      layer = "top";
+      position = "top";
+      modules-left = [
+        "custom/nixlogo"
+        "niri/workspaces"
+        "cpu"
+        "memory"
+      ];
+      modules-center = ["clock"];
+      modules-right = [
+        "tray"
+        "network"
+        "bluetooth"
+        "backlight"
+        "pulseaudio"
+        "battery"
+      ];
 
-        "niri/workspaces" = {};
-        "wlr/taskbar" = {
-          icon-size = 15;
-          on-click = "activate";
-          on-click-middle = "close";
-        };
-        tray = {
-          icon-size = 15;
-          spacing = 10;
-        };
+      # Logo
+      "custom/nixlogo" = {
+        format = "";
+        tooltip = false;
+        # on-click = "${pkgs.wofi}/bin/wofi --show drun";
+      };
 
-        idle_inhibitor = {
-          format = "{icon}";
-          format-icons = {
-            activated = " ";
-            deactivated = " ";
-          };
-        };
+      # Workspaces
+      "niri/workspaces" = {};
 
-        memory = {
-          format = " {percentage}%";
-          on-click = lib.getExe pkgs.resources;
-        };
+      # Cpu usage
+      cpu = {
+        interval = 5;
+        tooltip = false;
+        format = " {usage}%";
+        format-alt = " {load}";
 
-        backlight = {
-          format = "{icon}{percent}%";
-          format-icons = " ";
-          on-scroll-up = "${lib.getExe pkgs.brightnessctl} set +1%";
-          on-scroll-down = "${lib.getExe pkgs.brightnessctl} set 1%-";
+        states = {
+          warning = 70;
+          critical = 90;
         };
+      };
 
-        pulseaudio = {
-          format = "{icon}{volume}%";
-          format-bluetooth = " {volume}%";
-          format-muted = " -%";
-          format-source = " {volume}%";
-          format-source-muted = " ";
-          format-icons = {
-            default = [
-              " "
-              " "
-              " "
-            ];
-            headphone = " ";
-            headset = " ";
-            hands-free = " ";
-          };
-          on-click = "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-          on-click-right = lib.getExe pkgs.pwvucontrol;
-          tooltip-format = "{icon}{desc} {volume}%";
+      # Memory usage
+      memory = {
+        interval = 5;
+        format = " {percentage}%";
+        tooltip = " {used:0.1f}G/{total:0.1f}G";
+
+        states = {
+          warning = 70;
+          critical = 90;
         };
+      };
 
-        battery = {
-          states = {
-            warning = 30;
-            critical = 15;
-          };
-          format = "{icon}{capacity}%";
-          format-charging = " {capacity}%";
-          format-icons = [
-            " "
-            " "
-            " "
-            " "
-            " "
-          ];
-          tooltip-format = ''
-            {power}W
-            {timeTo}'';
-        };
-
-        clock = {
-          format = "{:%a %b %d %R}";
-          calendar.format = {
-            months = "<span color='${palette.error_color}'>{}</span>";
-            days = "<span color='${palette.accent_fg_color}'>{}</span>";
-            weeks = "<span color='${palette.success_color}'>W{}</span>";
-            weekdays = "<span color='${palette.warning_color}'>{}</span>";
-            today = "<span color='${palette.accent_color}'><u>{}</u></span>";
+      # Clock & Calendar
+      clock = {
+        format = "  {:%m/%d %I:%M %p}";
+        format-alt = "  {:%a, %d %b %Y}";
+        tooltip-format = "<tt>{calendar}</tt>";
+        calendar = {
+          mode = "year";
+          mode-mon-col = 3;
+          weeks-pos = "right";
+          on-scroll = 1;
+          format = {
+            months = "<span color='#ffead3'><b>{}</b></span>";
+            days = "<span color='#ecc6d9'><b>{}</b></span>";
+            weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+            weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+            today = "<span color='#ff6699'><b><u>{}</u></b></span>";
           };
           actions = {
             on-scroll-up = "shift_up";
             on-scroll-down = "shift_down";
           };
-          tooltip-format = "{calendar}";
         };
-      }
-    ];
+      };
+
+      # Tray
+      tray = {
+        icon-size = 15;
+        spacing = 8;
+      };
+
+      # Network
+      network = {
+        format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
+        format-wifi = "{icon}";
+        format-ethernet = "󰈀";
+        format-disconnected = "⚠";
+        tooltip-format-wifi = "WiFi: {essid} ({signalStrength}%)\n {bandwidthUpBytes}  {bandwidthDownBytes}";
+        tooltip-format-ethernet = "Ethernet: {ifname}\n {bandwidthUpBytes}  {bandwidthDownBytes}";
+        tooltip-format-disconnected = "Disconnected";
+        on-click = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+        interval = 5;
+      };
+
+      # Bluetooth
+      bluetooth = {
+        format = "󰂯";
+        format-disabled = "󰂲";
+        format-connected = "󰂱";
+        on-click = "blueberry";
+        interval = 5;
+      };
+
+      # Backlight
+      backlight = {
+        format-icons = ["" "" "" "" "" "" "" "" ""];
+        format = "<span color='#b4befe'>{icon}</span> {percent}%";
+        on-scroll-up = "brightnessctl set +1%";
+        on-scroll-down = "brightnessctl set -1%";
+      };
+
+      # Pulseaudio
+      pulseaudio = {
+        format = "<span color='#b4befe'>{icon}</span> {volume}%";
+        format-bluetooth = "󰂰";
+        tooltip-format = "Volume: {volume}%";
+        "format-muted" = "󰝟";
+        format-icons = {
+          default = ["󰖀" "󰕾" ""];
+          headphone = "";
+          headset = "";
+          hands-free = "";
+        };
+        on-click = "pavucontrol";
+      };
+
+      # Battery
+      battery = {
+        format-icons = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        format = "<span color='#737373'>{icon}</span> {capacity}%";
+        format-charging = "<span color='#b2f252'>󰂄</span> {capacity}%";
+        format-plugged = "<span color='#b2f252'>󰚥</span> MAX";
+        interval = 5;
+        tooltip-format = "{timeTo}, {capacity}%\n {power} W";
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+      };
+    };
     style = ''
+      @define-color bg #0d0d0d;
+      @define-color darkgrey #131822;
+      @define-color lightgrey #343a40;
+      @define-color red #e30b44;
+      @define-color white #f2efeb;
+      @define-color flake #d2fff3;
+      @define-color icyblue #aad1ee;
+      @define-color flakeicy #e5f0fa;
+      @define-color avocado #c1d98f;
+      @define-color lightyellow #f6eac4;
+      @define-color tray #181825;
+
       * {
+        border: none;
+        border-radius: 10px;
+        min-height: 0;
         font-family: Noto Sans Mono, Noto Sans Mono CJK SC, FiraCode Nerd Font;
-        font-weight: bold;
-        font-size: 14px;
+        font-size: 12px;
+        background: none;
       }
 
       window#waybar {
-        background: alpha(@theme_base_color, 0.9);
-        color: @theme_text_color;
+        background: transparent;
       }
 
-      #workspaces,
-      #taskbar button,
-      #mode,
-      #clock,
-      #tray,
-      #mpris,
-      #idle_inhibitor,
-      #backlight,
-      #cpu,
-      #memory,
-      #pulseaudio,
-      #battery {
-        padding: 0 6px;
+      tooltip {
+      border-radius: 8px;
+      padding: 15px;
+      background-color: @darkgrey;
+      }
+
+      tooltip label {
+      padding: 5px;
+      background-color: @darkgrey;
+      }
+
+      #workspaces {
+        margin: 6px 3px;
+        background: @bg;
       }
 
       #workspaces button {
-        padding: 3px 6px;
-      }
-      #workspaces button.focused,
-      #workspaces button.active {
-        color: ${palette.accent_color};
+        all: initial; /* Remove GTK theme values (waybar #1351) */
+        min-width: 0; /* Fix weird spacing in materia (waybar #450) */
+        box-shadow: inset 0 -3px transparent; /* Use box-shadow instead of border so the text isn't offset */
+        padding: 6px 10px;
+        border-radius: 6px;
+        background-color: @bg;
+        color: @lightgrey;
+        font-size: 10px;
       }
 
-      #battery.warning {
-        color: ${palette.warning_color};
+      #workspaces button.active {
+        color: @lightyellow;
       }
-      #battery.critical {
-        color: ${palette.error_color};
+
+      #workspaces button:hover {
+       box-shadow: inherit;
+       text-shadow: inherit;
+       color: @lightyellow;
+       background-color: @lightgrey;
       }
-      #battery.charging {
-        color: ${palette.success_color};
+
+      #temperature,
+      #cpu,
+      #memory,
+      #custom-power,
+      #custom-nixlogo,
+      #custom-weather,
+      #custom-pacman,
+      #backlight,
+      #pulseaudio,
+      #network,
+      #battery,
+      #backlight,
+      #bluetooth,
+      #clock,
+      #tray {
+        border-radius: 8px;
+        margin: 6px 2px;
+        padding: 6px 12px;
+        background-color: @bg;
+        color: @tray;
+      }
+
+      #temperature,
+      #cpu,
+      #memory,
+      #backlight,
+      #pulseaudio,
+      #network,
+      #battery,
+      #backlight,
+      #bluetooth{
+        color: @lightyellow;
+        background-color: @bg;
+      }
+
+      #clock{
+        font-family: JetBrainsMono Nerd Font;
+        background-color: @flake;
+      }
+
+      #custom-weather,
+      #custom-power {
+        background-color: @bg;
+        color: @lightyellow;
+      }
+
+      #custom-nixlogo {
+        font-size: 14px;
+        color: @icyblue;
       }
     '';
   };
