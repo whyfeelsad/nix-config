a461eae44926b87d1d7f33ddf51bd68272cf4f54
aaron <the.sad@niceboy.org>
2025-01-20 14:19:17 +0800

    feat: update niri config

--- modules/home/desktop/flatpak/default.nix
+++ modules/home/desktop/flatpak/default.nix
@@ -5,16 +5,3 @@
         {
           name = "flathub";
           location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        }
-      ];
-      update.auto = {
-        enable = true;
-        onCalendar = "weekly";
-      };
-      packages = [
-        "com.usebottles.bottles"
-        "app.zen_browser.zen"
-      ];
-    };
-  };
-}
