f48e630e92fa2ff83d47c9f51c54db211cbe31bc
aaron <the.sad@niceboy.org>
2025-01-20 09:49:57 +0800

    fish: add mvn alias

--- modules/home/cli/fish/default.nix
+++ modules/home/cli/fish/default.nix
@@ -6,6 +6,8 @@
       ll = "eza -l --time-style long-iso";
       la = "eza -la --time-style long-iso";
       tree = "eza --tree";
+
+      mvn = "/home/aaron/Documents/WorkSpace/apache-maven-3.8.4/bin/mvn";
     };
     interactiveShellInit = ''
       # Disable the greeting message.
