0036dcc6b96c9905026508ff86115c358b9324cc
Aaron <niceboy@duck.com>
2025-06-30 10:56:27 +0800

    feat: update config

--- flake.nix
+++ flake.nix
@@ -2,52 +2,24 @@
   description = "Aaron's NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    };
 
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    impermanence.url = "github:nix-community/impermanence";
+    preservation = {
+      url = "github:nix-community/preservation";
+    };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    daeuniverse.url = "github:daeuniverse/flake.nix";
   };
 
-  outputs = inputs @ {
-    disko,
-    nixpkgs,
-    daeuniverse,
-    impermanence,
-    home-manager,
-    ...
-  }: let
-    myvars = import ./vars;
-  in {
-    nixosConfigurations = {
-      mechrevo = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit myvars;};
-        modules = [
-          ./hosts/mechrevo
-          disko.nixosModules.disko
-          impermanence.nixosModules.impermanence
-          inputs.daeuniverse.nixosModules.daed
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            #            home-manager.extraSpecialArgs.flake-inputs = inputs;
-            home-manager.extraSpecialArgs = {inherit myvars;};
-            home-manager.users.aaron = import ./home;
-          }
-        ];
-      };
-    };
-  };
+  outputs = inputs: import ./hosts inputs;
 }
---
+++ hosts/default.nix
@@ -0,0 +1,30 @@
+inputs: let
+  myvars = import ../vars;
+
+  hmModules = [
+    inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        extraSpecialArgs = {inherit myvars;};
+        users.aaron = import ../home;
+      };
+    }
+  ];
+
+  mkSystem = host:
+    inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs myvars;};
+      modules =
+        [
+          ./${host}/configuration.nix
+        ]
+        ++ hmModules;
+    };
+in {
+  nixosConfigurations = {
+    mechrevo = mkSystem "mechrevo";
+  };
+}
