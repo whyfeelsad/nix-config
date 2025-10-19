7033d391a3856cd587a6e9fd28f428b04ec147c8
Aaron <niceboy@duck.com>
2025-10-19 20:34:42 +0800

    feat: update atuin

--- home/apps/atuin/default.nix
+++ home/apps/atuin/default.nix
@@ -6,7 +6,19 @@
     # synchronisation of your history between machines, via an Atuin server.
     atuin = {
       enable = true;
+      enableBashIntegration = true;
       enableFishIntegration = true;
+      daemon.enable = true;
+      settings = {
+        sync_frequency = 0;
+        inline_height = 10;
+        history_filter = [
+          ''^ls($|(\s+((-([a-zA-Z0-9]|-)+)|"(\.|[^/])[^"]*"|'(\.|[^/])[^']*'|(\.|[^/\s-])[^\s]*))*\s*$)'' # filter ls command with non-absolute pathes
+          ''^cd($|\s+('[^/][^']*'|"[^/][^"]*"|[^/\s'"][^\s]*))$'' # filter cd command with non-absolute pathes
+          ''/nix/store/.*'' # command contains /nix/store
+          ''--cookie[=\s]+.+'' # command contains cookie
+        ];
+      };
     };
   };
 }
