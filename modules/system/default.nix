c4821f08e869da252549529898e050b545893996
Aaron <niceboy@duck.com>
2026-01-11 17:13:20 +0800

    feat: 优化系统配置模块并添加时钟秒数显示

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,16 +1,28 @@
+###################################################################################
+#
+#  macOS's System configuration
+#
+#  All the configuration options are documented here:
+#    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+#  Incomplete list of macOS `defaults` commands :
+#    https://github.com/yannbertrand/macos-defaults
+#
+###################################################################################
+
 { config, ... }:
 {
-  # Add ability to used TouchID for sudo authentication
-  security.pam.services.sudo_local.touchIdAuth = true;
-
   # Set your time zone.
   time.timeZone = config.core'.timeZone;
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   system = {
     stateVersion = 6;
 
     defaults = {
       menuExtraClock.Show24Hour = true; # show 24 hour clock
+      menuExtraClock.ShowSeconds = true;
     };
   };
 }
